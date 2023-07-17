dbc5d8ef8f259d9a7b5167ae65847a78507044c2
websouldesign <training@websouldesign.com>
2023-07-17 13:18:11 +0530

    3 commit

--- src/Component/common/SideMenu.js
+++ src/Component/common/SideMenu.js
@@ -29,10 +29,10 @@ return (
           <h3 class="social-title">Follow Us</h3>
           <ul>
           
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Linkdin</a></li>
+            <li><a>Facebook</a></li>
+            <li><a >Twitter</a></li>
+            <li><a>Instagram</a></li>
+            <li><a >Linkdin</a></li>
           </ul>
         </div>
      
